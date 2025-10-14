74e211115eb282e29ff12687344d234b35b194b3
Winnie <516948708@qq.com>
2025-10-14 22:00:39 +0800

    fix: fix comment bug

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -63,10 +63,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: process.env.NEXT_PUBLIC_GISCUS_REPO ?? 'Klaus928/tailwind-css-starter-blog',
+      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID ?? 'R_kgDOQAXEKA',
+      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY ?? 'Announcements',
+      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID ?? 'DIC_kwDOQAXEKM4Cwn00',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
